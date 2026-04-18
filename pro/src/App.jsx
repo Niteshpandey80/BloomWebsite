bc1e34f639909eeeb8ad4e22474845986366f198
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-04-18 22:50:41 +0530

    Adding Page2

--- pro/src/App.jsx
+++ pro/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar.jsx'
 import Main from './components/main/Main.jsx'
 import Page1 from './components/page1/Page1.jsx'
+import Page2 from './components/page2/page2.jsx'
 
 
 const App = () => {
@@ -10,6 +11,7 @@ const App = () => {
      <Navbar/>
      <Main/>
      <Page1/>
+     <Page2/>
     </div>
   )
 }
---
+++ pro/src/components/page2/Page2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Page2 = () => {
+  return (
+    <div className='flex flex-row h-full w-full'>
+     
+    </div>
+  )
+}
+
+export default Page2
