870cb4f739aa0acc1eada4aeb99d5b6cb9be8855
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-04-18 23:43:26 +0530

    creating it

--- pro/src/components/page2/Page2.jsx
+++ pro/src/components/page2/Page2.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import img1 from '../../assets/im1.png'
+import img2 from '../../assets/im2.png'
+import img3 from '../../assets/im3.png'
+import img4 from '../../assets/im4.png'
+import './page2.css'
 
 const Page2 = () => {
   return (
-    <div className='flex flex-row h-full w-full'>
-      <div className='bg-red-500 h-full w-1/2'>
+    <div className='page2 flex flex-row h-full w-full '>
+      <div className=' h-full w-1/2'>
         <h1>Backend by the best companies </h1>
         <h1>andvisionary angles </h1>
       </div>
-      <div className='bg-green-600 h-full w-1/2'>
-            <h1>Helo </h1>
+      <div className=' h-full w-1/2 flex  '>
+            <img src={img1} alt=""  />
+            <img src={img2} alt="" />
+            <img src={img3} alt="" />
+            <img src={img4} alt="" />
+
       </div>
     </div>
   )
