583ef0aaecae4db77c8d59533daf9ccffc771320
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-04-19 01:15:32 +0530

    pages2 creation

--- pro/src/App.jsx
+++ pro/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Navbar from './components/navbar/Navbar.jsx'
 import Main from './components/main/Main.jsx'
 import Page1 from './components/page1/Page1.jsx'
-import Page2 from './components/page2/page2.jsx'
+import Page2 from './components/page2/Page2.jsx'
+
 
 
 const App = () => {
