79476e960af9a9678d143f36ee924c85d75745ff
Nitesh pandey <140426447+Niteshpandey80@users.noreply.github.com>
2026-04-18 22:51:10 +0530

    creating page using page2

--- pro/src/components/page2/Page2.jsx
+++ pro/src/components/page2/Page2.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const Page2 = () => {
   return (
     <div className='flex flex-row h-full w-full'>
-     
+      <div className='bg-red-500 h-full w-1/2'>
+        <h1>Backend by the best companies </h1>
+        <h1>andvisionary angles </h1>
+      </div>
+      <div className='bg-green-600 h-full w-1/2'>
+            <h1>Helo </h1>
+      </div>
     </div>
   )
 }
